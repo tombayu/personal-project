ae7c6669f00063a2bddf808d112e631659d029b1
tombayu <38533168+tombayu@users.noreply.github.com>
2020-05-03 18:13:41 +0200

    Data source geonames.org

--- toponimi/01_explore.R
+++ toponimi/01_explore.R
@@ -7,9 +7,9 @@ extrafont::loadfonts()
 
 rm(list = ls())
 
-toponimi <- st_read("data/ToponimiJadetabek.geojson")
-desa <- st_read("data/KelurahanJadetabek.geojson")
-kota <- st_read("data/KotaJadetabek.geojson")
+toponimi <- st_read("data/rbi/ToponimiJadetabek.geojson")
+desa <- st_read("data/rbi/KelurahanJadetabek.geojson")
+kota <- st_read("data/rbi/KotaJadetabek.geojson")
 # provinsi <- st_read("data/Provinsi.geojson") %>%
 #   filter(NAME_1 %in% c("Jawa Barat", "Jakarta Raya", "Banten"))
 
@@ -64,83 +64,34 @@ buat_peta <- function(lab, titik, title, subtitle, size = 2.5) {
 ## Kebon
 kebon <- filter_toponimi("Kebon", toponimi)
 buat_peta(kebon[[2]], kebon[[1]],
-          title = "Macam-macam 'Kebon' di Jadetabek", subtitle = "Nama tempat di Jadetabek dengan awalan 'kebon'")
-ggsave("output/01_Kebon.png", height = 13, width = 15, units = "cm", dpi = 400, bg = "transparent")
+          title = "Macam-macam 'Kebon' di Jadetabek", 
+          subtitle = "Nama tempat di Jadetabek dengan awalan 'kebon'")
+ggsave("output/rbi/01_Kebon.png", height = 13, width = 15, units = "cm", dpi = 400, bg = "transparent")
 
 ## Kampung
-indeks_kampung <- toponimi %>%
-  # filter(REMARK == "Wilayah Administrasi Kelurahan/Desa" | REMARK == "Permukiman dan Tempat Kegiatan") %>%
-  filter(str_detect(NAMOBJ, "Kampung")) %>%
-  filter(!str_detect(NAMOBJ, " ")) %>%
-  mutate(ID = 1:nrow(.)) %>%
-  st_drop_geometry() %>%
-  distinct(NAMOBJ, .keep_all = T) %>%
-  select(NAMOBJ, ID)
-
-kampung_all <- toponimi %>%
-  filter(str_detect(NAMOBJ, "Kampung")) %>%
-  filter(!str_detect(NAMOBJ, " "))
-
-kampung <- toponimi %>%
-  filter(str_detect(NAMOBJ, "Kampung")) %>%
-  filter(!str_detect(NAMOBJ, " ")) %>%
-  mutate(ID = 1:nrow(.)) %>%
-  filter(ID %in% indeks_kampung$ID) %>%
+kampung <- filter_toponimi("Kampung", toponimi)
+kampung[[2]] <- kampung[[2]] %>%
   mutate(NAMOBJ = gsub("Kampung", "", .$NAMOBJ))
-
-ggplot(kampung) +
-  geom_sf(data = kota, fill = "lightgrey", color = "white") +
-  geom_sf(data = kampung_all,aes(color = NAMOBJ), alpha = 0.5) +
-  geom_sf_text_repel(aes(label = NAMOBJ), family = "Titillium Web", size = 2.2) +
-  theme_ipsum_tw() +
-  labs(title = "Jakarta dan Kampungnya",
-       subtitle = "Nama wilayah di Jadetabek dengan awalan 'kampung-'",
-       caption = "© 2020 Tombayu Amadeo Hidayat\nSumber data: RBI 25K - BIG") +
-  theme(axis.title.x = element_blank(), axis.text.x = element_blank(), axis.ticks.x = element_blank(),
-        axis.title.y = element_blank(), axis.text.y = element_blank(), axis.ticks.y = element_blank(),
-        panel.grid.major = element_blank(), legend.position = "none")
-ggsave("output/02_Kampung.png", height = 13, width = 15, units = "cm", dpi = 400, bg = "transparent")
+buat_peta(kampung[[2]], kampung[[1]],
+          title = "Jakarta: Sebuah Kampung Besar",
+          subtitle = "Nama wilayah di Jadetabek dengan awalan 'kampung-'", size = 2.2)
+ggsave("output/rbi/02_Kampung.png", height = 13, width = 15, units = "cm", dpi = 400, bg = "transparent")
 
 ## Pondok
-indeks_pondok <- toponimi %>%
-  # filter(REMARK == "Wilayah Administrasi Kelurahan/Desa" | REMARK == "Permukiman dan Tempat Kegiatan") %>%
-  filter(str_detect(NAMOBJ, "Pondok")) %>%
-  filter(!str_detect(NAMOBJ, " ")) %>%
-  mutate(ID = 1:nrow(.)) %>%
-  st_drop_geometry() %>%
-  distinct(NAMOBJ, .keep_all = T) %>%
-  select(NAMOBJ, ID)
-
-pondok_all <- toponimi %>%
-  filter(str_detect(NAMOBJ, "Pondok")) %>%
-  filter(!str_detect(NAMOBJ, " "))
-
-pondok <- toponimi %>%
-  filter(str_detect(NAMOBJ, "Pondok")) %>%
-  filter(!str_detect(NAMOBJ, " ")) %>%
-  mutate(ID = 1:nrow(.)) %>%
-  filter(ID %in% indeks_pondok$ID) %>%
+pondok <- filter_toponimi("Pondok", toponimi)
+pondok[[2]] <- pondok[[2]] %>%
   mutate(NAMOBJ = gsub("Pondok", "", .$NAMOBJ))
-
-ggplot(pondok) +
-  geom_sf(data = kota, fill = "lightgrey", color = "white") +
-  geom_sf(data = pondok_all, aes(color = NAMOBJ), alpha = 0.5) +
-  geom_sf_text_repel(aes(label = NAMOBJ), family = "Titillium Web", size = 2.5) +
-  theme_ipsum_tw() +
-  labs(title = "Ada 'Pondok' Apa Saja di Jadetabek?",
-       subtitle = "Nama wilayah dengan awalan 'pondok-' di Jadetabek",
-       caption = "© 2020 Tombayu Amadeo Hidayat\nSumber data: RBI 25K - BIG") +
-  theme(axis.title.x = element_blank(), axis.text.x = element_blank(), axis.ticks.x = element_blank(),
-        axis.title.y = element_blank(), axis.text.y = element_blank(), axis.ticks.y = element_blank(),
-        panel.grid.major = element_blank(), legend.position = "none")
-ggsave("output/03_Pondok.png", height = 13, width = 15, units = "cm", dpi = 400, bg = "transparent")
+buat_peta(pondok[[2]], pondok[[1]],
+          title = "Ada 'Pondok' Apa Saja di Jadetabek?",
+          subtitle = "Nama wilayah dengan awalan 'pondok-' di Jadetabek", size = 2.2)
+ggsave("output/rbi/03_Pondok.png", height = 13, width = 15, units = "cm", dpi = 400, bg = "transparent")
 
 ## Jati
 jati <- filter_toponimi("Jati", toponimi)
 buat_peta(jati[[2]], jati[[1]],
           title = "Jati..?",
           subtitle = "Nama wilayah dengan awalan 'Jati-' di Jadetabek")
-ggsave("output/04_Jati.png", height = 13, width = 15, units = "cm", dpi = 400, bg = "transparent")
+ggsave("output/rbi/04_Jati.png", height = 13, width = 15, units = "cm", dpi = 400, bg = "transparent")
 
 ## Hari
 nama_hari <- c("Senin", "Selasa", "Rabu", "Rebo", "Kamis", "Jumat", "Sabtu", "Minggu") %>%
@@ -173,7 +124,7 @@ ggplot(hari) +
   theme(axis.title.x = element_blank(), axis.text.x = element_blank(), axis.ticks.x = element_blank(),
         axis.title.y = element_blank(), axis.text.y = element_blank(), axis.ticks.y = element_blank(),
         panel.grid.major = element_blank(), legend.position = "none")
-ggsave("output/05_Hari.png", height = 13, width = 15, units = "cm", dpi = 400, bg = "transparent")
+ggsave("output/rbi/05_Hari.png", height = 13, width = 15, units = "cm", dpi = 400, bg = "transparent")
 
 #### Fitur geografi!
 
@@ -181,38 +132,34 @@ ggsave("output/05_Hari.png", height = 13, width = 15, units = "cm", dpi = 400, b
 rawa <- filter_toponimi("Rawa", toponimi)
 buat_peta(rawa[[2]], rawa[[1]],
           title = "Rawa", subtitle = "") + theme(plot.subtitle = element_blank())
-ggsave("output/06_Rawa.png", height = 13, width = 15, units = "cm", dpi = 400, bg = "transparent")
+ggsave("output/rbi/06_Rawa.png", height = 13, width = 15, units = "cm", dpi = 400, bg = "transparent")
 
 ## Lebak
 lebak <- filter_toponimi("Lebak", toponimi)
 buat_peta(lebak[[2]], lebak[[1]],
           title = "Lebak", subtitle = "Wilayah dengan awalan lebak-") + theme(plot.subtitle = element_blank())
-ggsave("output/07_Lebak.png", height = 13, width = 15, units = "cm", dpi = 400, bg = "transparent")
+ggsave("output/rbi/07_Lebak.png", height = 13, width = 15, units = "cm", dpi = 400, bg = "transparent")
 
 ## Bukit
 bukit <- filter_toponimi("Bukit", toponimi)
 buat_peta(bukit[[2]], bukit[[1]],
           title = "Bukit", subtitle = "Wilayah dengan awalan bukit-") + theme(plot.subtitle = element_blank())
-ggsave("output/08_Bukit.png", height = 13, width = 15, units = "cm", dpi = 400, bg = "transparent")
+ggsave("output/rbi/08_Bukit.png", height = 13, width = 15, units = "cm", dpi = 400, bg = "transparent")
 
 ## Gunung
 gunung <- filter_toponimi("Gunung", toponimi)
 buat_peta(gunung[[2]], gunung[[1]],
           title = "Gunung", subtitle = "Wilayah dengan awalan gunung-") + theme(plot.subtitle = element_blank())
-ggsave("output/09_Gunung.png", height = 13, width = 15, units = "cm", dpi = 400, bg = "transparent")
+ggsave("output/rbi/09_Gunung.png", height = 13, width = 15, units = "cm", dpi = 400, bg = "transparent")
 
 ## Ci-
 ci <- filter_toponimi("Ci", toponimi)
 buat_peta(ci[[2]], ci[[1]],
           title = "Ci", subtitle = "Wilayah dengan awalan ci-", size = 2) + theme(plot.subtitle = element_blank())
-ggsave("output/10_Ci.png", height = 13, width = 15, units = "cm", dpi = 400, bg = "transparent")
+ggsave("output/rbi/10_Ci.png", height = 13, width = 15, units = "cm", dpi = 400, bg = "transparent")
 
 ## Tanah
 tanah <- filter_toponimi("Tanah", toponimi)
 buat_peta(tanah[[2]], tanah[[1]],
           title = "Tanah", subtitle = "Wilayah dengan awalan tanah-") + theme(plot.subtitle = element_blank())
-ggsave("output/11_Tanah.png", height = 13, width = 15, units = "cm", dpi = 400, bg = "transparent")
-
-## Try out
-read_tsv()
-ID <- read_tsv("D:/Spatial/ID/ID.txt", col_names = F)
+ggsave("output/rbi/11_Tanah.png", height = 13, width = 15, units = "cm", dpi = 400, bg = "transparent")
---
+++ toponimi/02_geonames.R
@@ -0,0 +1,194 @@
+pacman::p_load(magrittr, dplyr, sf, stringr, readr, ggplot2, ggthemes, purrr)
+library(ggsflabel)
+library(hrbrthemes)
+library(extrafont)
+
+rm(list = ls())
+
+# Load the geo-data
+desa <- st_read("data/rbi/KelurahanJadetabek.geojson")
+kota <- st_read("data/rbi/KotaJadetabek.geojson")
+
+# Load the gazetteers, convert to geo-data
+cols <- read_file("data/geonames/cols.txt") %>%
+  str_split("\r\n") %>%
+  unlist() %>%
+  str_split(":") %>%
+  map_chr(1) %>%
+  str_trim()
+
+gaz_jkt <- read_tsv("data/geonames/ID.txt", col_names = cols) %>%
+  st_as_sf(coords = c("longitude", "latitude")) %>%
+  st_set_crs(4326) %>%
+  st_intersection(kota)
+
+# Filter the names..
+# The function
+filter_toponimi <- function(pattern, toponimi = toponimi) {
+  indeks <- toponimi %>%
+    select(name, asciiname, alternatenames, feature.class, feature.code) %>%
+    filter(str_detect(name, regex(pattern, ignore_case = T)) | str_detect(alternatenames, regex(pattern, ignore_case = T))) %>%
+    filter(!str_detect(name, regex("kelurahan|satu|dua|tiga|empat|timur|barat|utara|selatan|indah|tengah", ignore_case = T))) %>%
+    # filter(!str_detect(name, regex("kelurahan|satu|dua|tiga|empat|timur|barat|indah|tengah", ignore_case = T))) %>%
+    mutate(shortname = gsub(paste0(".*", pattern), "", .$name, ignore.case = T)) %>%
+    mutate(shortname = tolower(gsub(" ", "", .$shortname)), id = 1:nrow(.)) %>%
+    st_drop_geometry() %>%
+    distinct(shortname, .keep_all = T) %>%
+    select(shortname, id)
+  
+  label <- toponimi %>%
+    select(name, asciiname, alternatenames, feature.class, feature.code) %>%
+    filter(str_detect(name, regex(pattern, ignore_case = T)) | str_detect(alternatenames, regex(pattern, ignore_case = T))) %>%
+    filter(!str_detect(name, regex("kelurahan|satu|dua|tiga|empat|timur|barat|utara|selatan|indah|tengah", ignore_case = T))) %>%
+    # filter(!str_detect(name, regex("kelurahan|satu|dua|tiga|empat|timur|barat|indah|tengah", ignore_case = T))) %>%
+    mutate(shortname = gsub(paste0(".*", pattern), "", .$name, ignore.case = T)) %>%
+    mutate(shortname = tolower(gsub(" ", "", .$shortname)), id = 1:nrow(.)) %>%
+    filter(id %in% indeks$id)
+  
+  titik <- toponimi %>%
+    select(name, asciiname, alternatenames, feature.class, feature.code) %>%
+    filter(str_detect(name, regex(pattern, ignore_case = T)) | str_detect(alternatenames, regex(pattern, ignore_case = T))) %>%
+    filter(!str_detect(name, regex("kelurahan", ignore_case = T))) %>%
+    # filter(!str_detect(name, regex("satu", ignore_case = T))) %>%
+    # filter(!str_detect(name, regex("dua", ignore_case = T))) %>%
+    mutate(shortname = gsub(paste0(".*", pattern), "", .$name, ignore.case = T)) %>%
+    mutate(shortname = tolower(gsub(" ", "", .$shortname)), id = 1:nrow(.))
+  
+  return(list(label = label, titik = titik))
+}
+
+buat_peta <- function(lab, titik, title, subtitle, size = 2.5) {
+  peta <- ggplot(lab) +
+    geom_sf(data = kota, fill = "lightgrey", color = "white") +
+    geom_sf(data = titik, aes(color = feature.class), alpha = 0.5) +
+    geom_sf_text_repel(aes(label = shortname), family = "Titillium Web", size = size) +
+    theme_ipsum_tw() +
+    labs(title = title,
+         subtitle = subtitle,
+         caption = "© 2020 Tombayu Amadeo Hidayat\nSumber data: geonames.org, RBI25K") +
+    theme(axis.title.x = element_blank(), axis.text.x = element_blank(), axis.ticks.x = element_blank(),
+          axis.title.y = element_blank(), axis.text.y = element_blank(), axis.ticks.y = element_blank(),
+          panel.grid.major = element_blank(), legend.position = "none")
+  return(peta)
+}
+
+# LET'S ROLL!
+
+## Kebon
+kebon <- filter_toponimi("kebon", gaz_jkt)
+buat_peta(kebon[[1]], kebon[[2]],
+          title = "Macam-macam 'Kebon' di Jadetabek", 
+          subtitle = "Toponimi dengan awalan 'kebon' di Jadetabek ")
+ggsave("output/geonames/01_Kebon.png", height = 13, width = 15, units = "cm", dpi = 500, bg = "transparent")
+
+## Kampung
+kampung <- filter_toponimi("kampung", gaz_jkt)
+buat_peta(kampung[[1]], kampung[[2]],
+          title = "Jakarta: Sebuah Kampung Besar",
+          subtitle = "Toponimi dengan awalan 'kampung-' di Jadetabek", size = 2)
+ggsave("output/geonames/02_Kampung.png", height = 13, width = 15, units = "cm", dpi = 500, bg = "transparent")
+
+## Pondok
+pondok <- filter_toponimi("pondok", gaz_jkt)
+pondok[[1]] <- pondok[[1]] %>%
+  filter(!str_detect(shortname, regex("pemakaman|-hilir|parhentian|-udik", ignore_case = T)))
+buat_peta(pondok[[1]], pondok[[2]],
+          title = "Ada 'Pondok' Apa Saja di Jadetabek?",
+          subtitle = "Toponimi dengan awalan 'pondok-' di Jadetabek", size = 2)
+ggsave("output/geonames/03_Pondok.png", height = 13, width = 15, units = "cm", dpi = 400, bg = "transparent")
+
+## Jati
+jati <- filter_toponimi("jati", gaz_jkt)
+buat_peta(jati[[1]], jati[[2]],
+          title = "Jati..?",
+          subtitle = "Nama wilayah dengan awalan 'Jati-' di Jadetabek")
+ggsave("output/geonames/04_Jati.png", height = 13, width = 15, units = "cm", dpi = 400, bg = "transparent")
+
+## Hari
+nama_hari <- c("senen", "senin", "selasa", "rabu", "rebo", "kamis", "jumat", "sabtu", "minggu")
+possible_filter <- possibly(filter_toponimi, otherwise = NA_real_)
+
+hari <- nama_hari %>%
+  map(possible_filter, toponimi = gaz_jkt) %>%
+  map("label") %>%
+  compact() %>%
+  reduce(rbind) %>%
+  filter(!str_detect(name, "Amaris")) %>%
+  mutate(shortname = R.utils::capitalize(tolower(gsub(" ", "", .$name))))
+
+semua_hari <- nama_hari %>%
+  map(possible_filter, toponimi = gaz_jkt) %>%
+  map("titik") %>%
+  compact() %>%
+  reduce(rbind)
+
+buat_peta(hari, semua_hari,
+          title = "Jakarta Hanya Mengenal 4 Hari..",
+          subtitle = "Toponimi dengan nama hari")
+ggsave("output/geonames/05_Hari.png", height = 13, width = 15, units = "cm", dpi = 400, bg = "transparent")
+
+## Fitur geografi!
+# Rawa
+rawa <- filter_toponimi("Rawa", gaz_jkt)
+buat_peta(rawa[[1]], rawa[[2]],
+          title = "Rawa", subtitle = "", size = 1.9) + theme(plot.subtitle = element_blank())
+ggsave("output/geonames/06_Rawa.png", height = 13, width = 15, units = "cm", dpi = 400, bg = "transparent")
+
+## Lebak
+lebak <- filter_toponimi("Lebak", gaz_jkt)
+buat_peta(lebak[[1]], lebak[[2]],
+          title = "Lebak", subtitle = "Wilayah dengan awalan lebak-") + theme(plot.subtitle = element_blank())
+ggsave("output/geonames/07_Lebak.png", height = 13, width = 15, units = "cm", dpi = 400, bg = "transparent")
+
+## Bukit
+bukit <- filter_toponimi("Bukit", gaz_jkt)
+buat_peta(bukit[[1]], bukit[[2]],
+          title = "Bukit", subtitle = "Wilayah dengan awalan bukit-") + theme(plot.subtitle = element_blank())
+ggsave("output/geonames/08_Bukit.png", height = 13, width = 15, units = "cm", dpi = 400, bg = "transparent")
+
+## Gunung
+gunung <- filter_toponimi("Gunung", gaz_jkt)
+add <- gunung[[2]] %>% filter(name == "Gunung") %>% filter(feature.code == "PPLX")
+gunung[[1]] <- rbind(gunung[[1]], add)
+gunung[[1]] <- gunung[[1]] %>%
+  mutate(shortname = R.utils::capitalize(tolower(gsub(" ", "", .$name))))
+
+buat_peta(gunung[[1]], gunung[[2]],
+          title = "Gunung", subtitle = "Wilayah dengan awalan gunung-") + theme(plot.subtitle = element_blank())
+ggsave("output/geonames/09_Gunung.png", height = 13, width = 15, units = "cm", dpi = 400, bg = "transparent")
+
+## Tanah
+tanah <- filter_toponimi("Tanah", gaz_jkt)
+buat_peta(tanah[[1]], tanah[[2]],
+          title = "Tanah", subtitle = "Wilayah dengan awalan tanah-") + theme(plot.subtitle = element_blank())
+ggsave("output/geonames/10_Tanah.png", height = 13, width = 15, units = "cm", dpi = 400, bg = "transparent")
+
+##############
+
+
+id <- gaz_jkt %>%
+  select(name, asciiname, alternatenames, feature.class, feature.code) %>%
+  filter(str_detect(name, regex("kampung", ignore_case = T)) | str_detect(alternatenames, regex("kampung", ignore_case = T))) %>%
+  filter(!str_detect(name, "kelurahan")) %>%
+  mutate(shortname = gsub(".*kampung", "", .$name, ignore.case = T)) %>%
+  mutate(shortname = tolower(gsub(" ", "", .$shortname)), id = 1:nrow(.)) %>%
+  st_drop_geometry() %>%
+  distinct(shortname, .keep_all = T) %>%
+  select(shortname, id)
+
+label <- gaz_jkt %>%
+  select(name, asciiname, alternatenames, feature.class, feature.code) %>%
+  filter(str_detect(name, regex("kampung", ignore_case = T)) | str_detect(alternatenames, regex("kampung", ignore_case = T))) %>%
+  filter(!str_detect(name, "kelurahan")) %>%
+  mutate(shortname = gsub(".*kampung", "", .$name, ignore.case = T)) %>%
+  mutate(shortname = tolower(gsub(" ", "", .$shortname)), id = 1:nrow(.))
+
+titik <- gaz_jkt %>%
+  select(name, asciiname, alternatenames, feature.class, feature.code) %>%
+  filter(str_detect(name, regex("kampung", ignore_case = T)) | str_detect(alternatenames, regex("kampung", ignore_case = T))) %>%
+  filter(!str_detect(name, "kelurahan"))
+id  
+label
+
+?str_detect
+kampung
