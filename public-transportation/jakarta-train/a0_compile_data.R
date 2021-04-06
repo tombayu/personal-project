eb85dedb898231ee167d14332c23da79e6727eac
tombayu <38533168+tombayu@users.noreply.github.com>
2021-04-06 19:33:40 +0200

    Train map update

--- public-transportation/jakarta-train/a0_compile_data.R
+++ public-transportation/jakarta-train/a0_compile_data.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(sf)
+library(googleway)
 
 # Load the self-compiled data
 lines <- readxl::read_xlsx("data/rekap.xlsx")
@@ -7,6 +8,18 @@ lines <- readxl::read_xlsx("data/rekap.xlsx")
 # Load Trafi-scraped data
 dt_trafi <- read_rds("data/trains_jkt.rds")
 
-# Load SSC data
-"data/future-network.kml"
-"data/current-network.kml"
+rute <- dt_trafi %>%
+  select(-info_rute, -stasiun) %>%
+  unnest(cols = c(rute)) %>%
+  mutate(koordinat = map(shape, decode_pl)) %>%
+  unnest(koordinat) %>%
+  st_as_sf(coords = c("lon", "lat")) %>%
+  group_by_at(vars(-stops, -geometry)) %>%
+  summarise(do_union = F) %>%
+  st_cast("LINESTRING") %>%
+  select(-shape, -geometry)
+st_crs(rute) <- 4326
+
+st_write(rute, "data/raw/rute_trafi.geojson")
+
+
